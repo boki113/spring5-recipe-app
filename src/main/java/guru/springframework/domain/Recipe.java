2eb31bb7d9fc8984946280ff874fb0edbdff2c9b
Boris Perica <borisperper@gmail.com>
2020-11-28 17:57:45 +0100

    added domain entities

---
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -0,0 +1,121 @@
+package guru.springframework.domain;
+
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+
+/**
+ * 
+ * @author perica
+ *
+ */
+@Entity
+public class Recipe {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String description;
+	private Integer prepTime;
+	private Integer cookTime;
+	private String source;
+	private String url;
+	private String directions;
+	
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe" )
+	private Set<Ingrediant> ingrediants;
+	
+	@Lob
+	private Byte[] image;
+	
+	@OneToOne(cascade = CascadeType.ALL)
+	private Notes notes;
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Notes getNotes() {
+		return notes;
+	}
+
+	public void setNotes(Notes notes) {
+		this.notes = notes;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Integer getPrepTime() {
+		return prepTime;
+	}
+
+	public void setPrepTime(Integer prepTime) {
+		this.prepTime = prepTime;
+	}
+
+	public Integer getCookTime() {
+		return cookTime;
+	}
+
+	public void setCookTime(Integer cookTime) {
+		this.cookTime = cookTime;
+	}
+
+	public String getSource() {
+		return source;
+	}
+
+	public void setSource(String source) {
+		this.source = source;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public String getDirections() {
+		return directions;
+	}
+
+	public void setDirections(String directions) {
+		this.directions = directions;
+	}
+
+	public Byte[] getImage() {
+		return image;
+	}
+
+	public void setImage(Byte[] image) {
+		this.image = image;
+	}
+
+	public Set<Ingrediant> getIngrediants() {
+		return ingrediants;
+	}
+
+	public void setIngrediants(Set<Ingrediant> ingrediants) {
+		this.ingrediants = ingrediants;
+	}
+}
